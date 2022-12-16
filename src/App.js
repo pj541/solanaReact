96b9db2ccbbc38fc53abbe199fa0f69287231e48
pj541 <198r1a0541@cmrec.ac.in>
2022-12-16 20:14:17 +0530

    set up a successful connection portal

--- src/App.js
+++ src/App.js
@@ -1,40 +1,69 @@
 import './App.css';
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import gitlogo from './assets/gitlogo.svg';
 // Constants
 const GIT_HANDLE = '/pj541';
 const GIT_LINK = `https://github.com/${GIT_HANDLE}`;
 
-const connectwalletmethod = async ()=>{
-
-}
-
-const ifnotConnected = () =>{
-  return (<button
-  className="cta-button connect-wallet-button"
-  onClick={connectwalletmethod()}>
-    Connect to Wallet
-  </button>);
-}
-const walletConnected = async ()=>{
-  if(window?.solana?.isPhantom){
-    console.log("Phantom Found");
-    const solana = window.solana;
-    const rep = await solana.connect({onlyIfTrusted: true});
-    console.log(rep.publicKey.toString());
-  }
-  else{
-    alert("Where is your phantom? ");
-  }
-}
 
 const App = () => {
+  const [walletAddress, setwalletAddress]= useState(null);
+  const [phantomfound, setPhantom] = useState(null);
+  const walletConnected = async ()=>{
+    if(window?.solana?.isPhantom){
+      console.log("Phantom Found");
+      setPhantom('Found');
+      
+      const solana = window.solana;
+      const rep = await solana.connect({onlyIfTrusted: true});
+      console.log(rep.publicKey.toString());
+      setwalletAddress(rep.publicKey.toString());
+    }
+  }
+
+  const getPhantom = async()=>{
+    if(window?.solana?.isPhantom){
+      setPhantom('Found');
+      
+    }else{
+      const url = "https://phantom.app/download";
+      window.open(url, '_blank');
+    }
+  }
+
+  const phantomnotfound = () =>{
+    if(window?.solana?.isPhantom){
+      setPhantom('Found');
+      return -1;
+    }
+    return (<button
+      className="cta-button connect-wallet-button"
+      onClick={getPhantom}>
+        Enable Phantom 
+      </button>);
+  }
+
+  const connectwalletmethod = async ()=>{
+    const solana = window.solana;
+    const rep = await solana.connect();
+    setwalletAddress(rep.publicKey.toString());
+  }
+  
+  const ifnotConnected = () =>{
+    return (<button
+    className="cta-button connect-wallet-button"
+    onClick={connectwalletmethod}>
+      Connect to Wallet
+    </button>);
+  }  
+
   useEffect(() => {
     const onload = async ()=>{
       try{
         await walletConnected();
+        
       }catch(err){
-        console.log(`THE ERROR IS ${err}`);
+        console.log(err);
       }
     }
     window.addEventListener('load', onload);
@@ -42,13 +71,14 @@ const App = () => {
   },[]);
   return (
     <div className="App">
-      <div className="container">
+      <div className={walletAddress ? 'authed-container' : 'container'}>
         <div className="header-container">
-          <p className="header">💃 GIF Portal</p>
+          <p className="header">{phantomfound ? '💃 GIF Portal': 'Wheres your Phantom ?'}</p>
           <p className="sub-text">
             View your GIF collection in the metaverse ✨
           </p>
-          {ifnotConnected()}
+          { !phantomfound && phantomnotfound()}
+          { phantomfound &&(!walletAddress) && ifnotConnected()}
 
         </div>
         <div className="footer-container">
